d9ab065a6318eaeb3a156ced619eee7762024f50
Matthew Ong <matthew.ong@westonrobot.com>
2024-04-12 11:57:33 +0800

    nav_bringup: copied skeleton from nav2_bringup

---
+++ README.md
@@ -0,0 +1,63 @@
+# WR Devkit Navigation
+
+![default workflow](https://github.com/westonrobot/wr_devkit_navigation/actions/workflows/default.yml/badge.svg?branch=main)
+
+This repository provides a reference setup for using Nav2 stack to do 2D navigation with the mobile robot development kit from Weston Robot.
+
+## Requirements
+
+The following hardware configurations are supported: 
+
+* WR Devkit V1.0
+  * With Livox Mid-360 Lidar
+
+The onboard computer with the devkit should have been configured with the following software environment:
+
+* Ubuntu 22.04 
+* ROS Humble
+
+## Installation
+
+Please refer to the [CI build script](.github/workflows/default.yml) for the most complete and up-to-date installation steps.
+
+* Install the Weston Robot Platform SDK (wrp-sdk)
+
+    ```bash
+    $ sudo install -m 0755 -d /etc/apt/keyrings
+    $ curl -fsSL http://deb.westonrobot.net/signing.key | sudo gpg --dearmor -o /etc/apt/keyrings/weston-robot.gpg
+    $ sudo chmod a+r /etc/apt/keyrings/weston-robot.gpg
+
+    $ echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/weston-robot.gpg] http://deb.westonrobot.net/$(lsb_release -cs) $(lsb_release -cs) main" | \
+        sudo tee /etc/apt/sources.list.d/weston-robot.list > /dev/null
+    $ sudo apt-get update
+
+    $ sudo apt-get install wrp-sdk
+    ```
+
+    Please refer to [this page](https://docs.westonrobot.net/software/installation_guide.html) for more details of the installation steps.
+
+* Install Livox SDK2 (if you have the devkit variant with the Livox Mid-360 Lidar)
+
+    ```bash
+    $ cd ~
+    $ git clone https://github.com/Livox-SDK/Livox-SDK2.git
+    $ cd Livox-SDK2
+    $ mkdir build && cd build && cmake .. && make
+    $ sudo make install
+    ```
+
+    Note: you can build and install the Livox-SDK2 at your preferred places other than "~/Livox-SDK2". And you can optionally remove the "Livox-SDK2" folder after installation.
+
+* Import the ROS packages into the workspace and build
+
+    ```bash
+    $ cd <your-workspace>/wr_devkit_navigation
+    # for Livox Mid-360 setup
+    $ vcs import src < ./navigation.repos
+
+    $ source /opt/ros/humble/setup.bash
+    $ colcon build --symlink-install
+    ```
+
+    The build process should finish without any errors.
---
+++ src/wr_devkit_nav_bringup/launch/wr_devkit_nav_bringup.launch.py
@@ -0,0 +1,231 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, GroupAction, SetEnvironmentVariable
+from launch.conditions import IfCondition
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import LoadComposableNodes, SetParameter, Node, PushRosNamespace
+from launch_ros.descriptions import ComposableNode, ParameterFile
+from nav2_common.launch import RewrittenYaml, ReplaceString
+
+
+def generate_launch_description():
+    # Get the launch directory
+    bringup_dir = get_package_share_directory("wr_devkit_nav_bringup")
+
+    use_namespace = LaunchConfiguration("use_namespace")
+    namespace = LaunchConfiguration("namespace")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+    autostart = LaunchConfiguration("autostart")
+    params_file = LaunchConfiguration("params_file")
+    map_yaml_file = LaunchConfiguration('map')
+    container_name = LaunchConfiguration("container_name")
+    container_name_full = (namespace, "/", container_name)
+    log_level = LaunchConfiguration("log_level")
+
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
+    # https://github.com/ros/geometry2/issues/32
+    # https://github.com/ros/robot_state_publisher/pull/30
+    # TODO(orduno) Substitute with `PushNodeRemapping`
+    #              https://github.com/ros2/launch_ros/issues/56
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {"use_sim_time": use_sim_time, "yaml_filename": map_yaml_file}
+
+    lifecycle_nodes = [
+        "controller_server",
+        "smoother_server",
+        "planner_server",
+        "behavior_server",
+        "velocity_smoother",
+        "collision_monitor",
+        "bt_navigator",
+        "waypoint_follower",
+    ]
+
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
+    # https://github.com/ros/geometry2/issues/32
+    # https://github.com/ros/robot_state_publisher/pull/30
+    # TODO(orduno) Substitute with `PushNodeRemapping`
+    #              https://github.com/ros2/launch_ros/issues/56
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+
+    # Create our own temporary YAML files that include substitutions
+    param_substitutions = {"autostart": autostart}
+
+    # Only it applys when `use_namespace` is True.
+    # '<robot_namespace>' keyword shall be replaced by 'namespace' launch argument
+    # in config file 'nav2_multirobot_params.yaml' as a default & example.
+    # User defined config file should contain '<robot_namespace>' keyword for the replacements.
+    params_file = ReplaceString(
+        source_file=params_file,
+        replacements={'<robot_namespace>': ('/', namespace)},
+        condition=IfCondition(use_namespace))
+
+    configured_params = ParameterFile(
+        RewrittenYaml(
+            source_file=params_file,
+            root_key=namespace,
+            param_rewrites=param_substitutions,
+            convert_types=True,
+        ),
+        allow_substs=True,
+    )
+
+    stdout_linebuf_envvar = SetEnvironmentVariable(
+        "RCUTILS_LOGGING_BUFFERED_STREAM", "1"
+    )
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        "use_namespace",
+        default_value="false",
+        description="Whether to apply a namespace to the navigation nodes",
+    )
+
+    declare_namespace_cmd = DeclareLaunchArgument(
+        "namespace", default_value="", description="Top-level namespace"
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        "use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        "params_file",
+        default_value=os.path.join(
+            bringup_dir, "params", "nav2_params_ranger_mini.yaml"
+        ),
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        'map',
+        description='Full path to map yaml file to load')
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        "autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    )
+
+    declare_container_name_cmd = DeclareLaunchArgument(
+        "container_name",
+        default_value="nav2_container",
+        description="the name of conatiner that nodes will load in if use composition",
+    )
+
+    declare_log_level_cmd = DeclareLaunchArgument(
+        "log_level", default_value="info", description="log level"
+    )
+
+    load_composable_nodes = GroupAction(
+        actions=[
+            SetParameter("use_sim_time", use_sim_time),
+            PushRosNamespace(condition=IfCondition(use_namespace), namespace=namespace),
+            Node(
+                name="nav2_container",
+                package="rclcpp_components",
+                executable="component_container_isolated",
+                parameters=[configured_params, {"autostart": autostart}],
+                arguments=["--ros-args", "--log-level", log_level],
+                remappings=remappings,
+                output="screen",
+            ),
+            LoadComposableNodes(
+                target_container=container_name_full,
+                composable_node_descriptions=[
+                    ComposableNode(
+                        package="nav2_controller",
+                        plugin="nav2_controller::ControllerServer",
+                        name="controller_server",
+                        parameters=[configured_params],
+                        remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
+                    ),
+                    ComposableNode(
+                        package="nav2_smoother",
+                        plugin="nav2_smoother::SmootherServer",
+                        name="smoother_server",
+                        parameters=[configured_params],
+                        remappings=remappings,
+                    ),
+                    ComposableNode(
+                        package="nav2_planner",
+                        plugin="nav2_planner::PlannerServer",
+                        name="planner_server",
+                        parameters=[configured_params],
+                        remappings=remappings,
+                    ),
+                    ComposableNode(
+                        package="nav2_behaviors",
+                        plugin="behavior_server::BehaviorServer",
+                        name="behavior_server",
+                        parameters=[configured_params],
+                        remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
+                    ),
+                    ComposableNode(
+                        package="nav2_bt_navigator",
+                        plugin="nav2_bt_navigator::BtNavigator",
+                        name="bt_navigator",
+                        parameters=[configured_params],
+                        remappings=remappings,
+                    ),
+                    ComposableNode(
+                        package="nav2_waypoint_follower",
+                        plugin="nav2_waypoint_follower::WaypointFollower",
+                        name="waypoint_follower",
+                        parameters=[configured_params],
+                        remappings=remappings,
+                    ),
+                    ComposableNode(
+                        package="nav2_velocity_smoother",
+                        plugin="nav2_velocity_smoother::VelocitySmoother",
+                        name="velocity_smoother",
+                        parameters=[configured_params],
+                        remappings=remappings + [("cmd_vel", "cmd_vel_nav")],
+                    ),
+                    ComposableNode(
+                        package="nav2_collision_monitor",
+                        plugin="nav2_collision_monitor::CollisionMonitor",
+                        name="collision_monitor",
+                        parameters=[configured_params],
+                        remappings=remappings,
+                    ),
+                    ComposableNode(
+                        package="nav2_lifecycle_manager",
+                        plugin="nav2_lifecycle_manager::LifecycleManager",
+                        name="lifecycle_manager_navigation",
+                        parameters=[
+                            {"autostart": autostart, "node_names": lifecycle_nodes}
+                        ],
+                    ),
+                ],
+            ),
+        ],
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Set environment variables
+    ld.add_action(stdout_linebuf_envvar)
+
+    # Declare the launch options
+    ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_container_name_cmd)
+    ld.add_action(declare_log_level_cmd)
+    # Add the actions to launch all of the navigation nodes
+    ld.add_action(load_composable_nodes)
+
+    return ld
