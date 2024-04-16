dc88fbeb2da1aa4e8c073ee53dab1f641acf97d5
Matthew Ong <matthew.ong@westonrobot.com>
2024-04-16 18:16:23 +0800

    robot_bringup: to fix ranger bringup

--- src/wr_devkit_robot_bringup/launch/wr_devkit_robot_bringup.launch.py
+++ src/wr_devkit_robot_bringup/launch/wr_devkit_robot_bringup.launch.py
@@ -5,6 +5,7 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription, GroupAction, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch_ros.actions import Node, SetParameter, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
@@ -12,14 +13,11 @@ from launch.conditions import IfCondition
 
 
 def generate_launch_description():
-    description_pkg_path = get_package_share_directory("wr_devkit_description")
-    livox_pkg_path = get_package_share_directory("livox_ros_driver2")
-    wrp_ros2_pkg_path = get_package_share_directory("wrp_ros2")
-
     use_namespace = LaunchConfiguration("use_namespace")
     namespace = LaunchConfiguration("namespace")
     use_sim_time = LaunchConfiguration("use_sim_time")
     imu_path = LaunchConfiguration("imu_path")
+    robot_base = LaunchConfiguration("robot_base")
 
     declare_use_namespace_cmd = DeclareLaunchArgument(
         "use_namespace",
@@ -43,24 +41,13 @@ def generate_launch_description():
         description="Path to IMU port",
     )
 
-    description_bringup = GroupAction(
-        [
-            PushRosNamespace(condition=IfCondition(use_namespace), namespace=namespace),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource(
-                    [
-                        os.path.join(
-                            get_package_share_directory("wr_devkit_description"),
-                            "launch",
-                            "wr_devkit_description.launch.py",
-                        )
-                    ]
-                ),
-                launch_arguments={}.items(),
-            ),]
+    declare_robot_base_cmd = DeclareLaunchArgument(
+        "robot_base",
+        default_value="ranger_mini_v2",
+        description="Robot base model",
     )
 
-    livox_bringup = GroupAction(
+    robot_bringup = GroupAction(
         [
             PushRosNamespace(condition=IfCondition(use_namespace), namespace=namespace),
             IncludeLaunchDescription(
@@ -69,56 +56,42 @@ def generate_launch_description():
                         os.path.join(
                             get_package_share_directory("wr_devkit_robot_bringup"),
                             "launch",
-                            "mid360.launch.py",
+                            "wr_devkit_sensor_bringup.launch.py",
                         )
                     ]
                 ),
-                launch_arguments={}.items(),
+                launch_arguments={
+                    "use_namespace": use_namespace,
+                    "namespace": namespace,
+                    "use_sim_time": use_sim_time,
+                    "imu_path": imu_path,
+                }.items(),
+            ),
+            IncludeLaunchDescription(
+                XMLLaunchDescriptionSource(
+                    [
+                        os.path.join(
+                            get_package_share_directory("ranger_bringup"),
+                            "launch",
+                            "ranger_mini_v2.launch.xml",
+                        )
+                    ]
+                ),
+                launch_arguments={
+                    "publish_odom_tf": True,
+                }.items(),
             ),
         ]
     )
 
-    imu_bringup = GroupAction([
-        PushRosNamespace(condition=IfCondition(use_namespace), namespace=namespace),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(
-                os.path.join(
-                    wrp_ros2_pkg_path, "launch/peripheral", "imu_sensor.launch.py"
-                )
-            ),
-            launch_arguments={
-                "use_sim_time": use_sim_time,
-                "device_path": "/dev/ttyUSB0",
-                "baud_rate": "115200",
-                "frame_id": "ch104m_imu_frame",
-                "sensor_model": "hipnuc",
-            }.items(),
-        ),
-    ])
-
-    pointcloud_to_laserscan_node = Node(
-        package="pointcloud_to_laserscan",
-        executable="pointcloud_to_laserscan_node",
-        name="pointcloud_to_laserscan_node",
-        output="screen",
-        parameters=[
-            {"use_sim_time": use_sim_time},
-            {"range_min": 0.1},
-            {"range_max": 100.0},
-            {"scan_time": 0.1},
-            {"min_height": -0.025},
-            {"max_height": 0.025},
-            {"angle_min": -3.14159265},
-            {"angle_max": 3.14159265},
-            {"angle_increment": 0.0174532925},
-            {"inf_epsilon": 1.0},
-            {"tf_tolerance": 0.03},
-        ],
-        remappings=[
-            ("/cloud_in", "/livox/lidar"),
-            ("/scan", "/scan"),
-        ],
-    )
+    static_tf = Node(
+            package='tf2_ros',
+            executable='static_transform_publisher',
+            name='devkit_transform_publisher',
+            arguments=['--x', '0.0', '--y', '-0.0', '--z', '0.25',
+                       '--yaw', '0', '--pitch', '0', '--roll', '0',
+                       '--frame-id', 'base_link', '--child-frame-id', 'wr_devkit_base_link']
+        )
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -128,11 +101,10 @@ def generate_launch_description():
     ld.add_action(declare_namespace_cmd)
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_imu_path_cmd)
+    ld.add_action(declare_robot_base_cmd)
 
     # Add the actions to launch all of the navigation nodes
-    ld.add_action(description_bringup)
-    ld.add_action(livox_bringup)
-    ld.add_action(imu_bringup)
-    ld.add_action(pointcloud_to_laserscan_node)
+    ld.add_action(robot_bringup)
+    ld.add_action(static_tf)
 
     return ld
