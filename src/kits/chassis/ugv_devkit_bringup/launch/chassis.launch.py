cb9bd6d0247a1683d3eac8aeae5f3e51345415e9
kanngweii <kangwei.pang@westonrobot.com>
2025-03-17 11:34:46 +0800

    kits: chassis: launch: Modify launch file to take in cli argument

--- src/kits/chassis/ugv_devkit_bringup/launch/chassis.launch.py
+++ src/kits/chassis/ugv_devkit_bringup/launch/chassis.launch.py
@@ -1,13 +1,44 @@
-from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, GroupAction, DeclareLaunchArgument
-from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch import LaunchDescription, LaunchContext
+from launch.launch_context import LaunchContext
+from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node, PushRosNamespace
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch.substitution import Substitution
+from launch.substitutions import LaunchConfiguration
 from launch.conditions import IfCondition
+from launch.some_substitutions_type import SomeSubstitutionsType
+from launch.utilities import normalize_to_list_of_substitutions
+from launch.utilities import perform_substitutions
+from typing import Dict, Text
 
 import os
 import xacro
+
+class Xacro(Substitution):
+    def __init__(self, file_path: SomeSubstitutionsType, mappings: Dict[str, SomeSubstitutionsType] = {}, verbose: bool = False):
+        super().__init__()
+        self.__file_path = normalize_to_list_of_substitutions(file_path)
+        self.__mappings = {key: normalize_to_list_of_substitutions(
+            value) for key, value in mappings.items()}
+        self.__verbose = verbose
+
+    def describe(self) -> Text:
+        return f"Xacro: {self.__file_path}"
+
+    def perform(self, context: LaunchContext) -> Text:
+        file_path = perform_substitutions(context, self.__file_path)
+        mappings = {key: perform_substitutions(
+            context, value) for key, value in self.__mappings.items()}
+
+        if self.__verbose:
+            print(f"xacro file_path: {file_path}")
+            print(f"xacro mappings: {mappings}")
+        document = xacro.process_file(file_path, mappings=mappings)
+        document_string = document.toprettyxml(indent="  ")
+
+        if self.__verbose:
+            print(f"xacro result: {document_string}")
+        return document_string
 
 
 def generate_launch_description():
@@ -24,6 +55,12 @@ def generate_launch_description():
         description="Top-level namespace"
     )
 
+    declare_chassis_extension_arg = DeclareLaunchArgument(
+        "chassis_extension",
+        default_value="false",
+        description="UGV Devkit Chassis Extension V1.1"
+    )
+
     # --------- Namespace ---------
     PushRosNamespace(
         condition=IfCondition(LaunchConfiguration("use_namespace")),
@@ -36,15 +73,19 @@ def generate_launch_description():
             "ugv_devkit_bringup"),
         "urdf/chassis.urdf.xacro"
     )
-    doc = xacro.process_file(xacro_file)
-    robot_desc = doc.toprettyxml(indent='  ')
 
     load_description = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         name="ugv_devkit_sensor_kit_state_publisher",
         output="screen",
-        parameters=[{"robot_description": robot_desc}],
+        parameters=[
+            {"robot_description": Xacro(
+                file_path=xacro_file,
+                mappings={"chassis_ext": LaunchConfiguration(
+                    "chassis_extension")}
+            )}
+        ],
         remappings=[
             ("robot_description", "ugv_devkit_chassis_description"),
         ]
@@ -53,5 +94,6 @@ def generate_launch_description():
     return LaunchDescription([
         declare_use_namespace_arg,
         declare_namespace_arg,
+        declare_chassis_extension_arg,
         load_description,
     ])
